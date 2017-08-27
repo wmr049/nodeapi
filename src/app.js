5ab4eaada5d5fa1083652baa99bd21da55e77359
Milton Reis <wmr049@gmail.com>
2017-08-27 06:42:48 -0300

    Criado CRUD como mongoose

---
+++ src/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+
+//Conectar ao banco de dados
+mongoose.connect('mongodb://wmr049:wmr049@ds036079.mlab.com:36079/ndstr')
+
+//Carregar os Models
+const Product = require('./models/product');
+
+//Carrega as Rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
+
---
+++ src/models/product.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'O titulo é obrigatório'],
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: [true, 'o slug é obrigatório'],
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: [true, 'A descrição é obrigatória'],
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: [true, 'O preço é obrigatório']
+    },
+    active: {
+        type: Boolean,
+        required: [true, 'O Status é obrigatório'],
+        default: true
+    },
+    tags: [{
+        type: String,
+        required: [true, 'As tags são obrigatório']
+    }],
+});
+
+module.exports = mongoose.model('Product', schema);
