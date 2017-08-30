225f9d6111394f144a59fdb9363dd5c4e0e149d7
Milton Reis <milton.reis@kroton.com.br>
2017-08-30 17:13:46 -0300

    Alteração do ApiKey sendgrid

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://wmr049:wmr049@ds036079.mlab.com:36079/ndstr',
-    sendgridKey: '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',
+    sendgridKey: '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',
     containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=repofiles;AccountKey=TBbTT4dudyD5Gj/Z1OdblKM0ZTptanKwBiRMLiMGaYIYAZAJcAxPH0ZP9PJjd2CR6B8zEVLe0W23aaqq45X6sQ==;EndpointSuffix=core.windows.net'
 }
